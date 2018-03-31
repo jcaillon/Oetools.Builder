03dbe9ef926293a2e8e6857e208166df044063b1
Julien Caillon <julien.caillon@gmail.com>
2018-03-31 21:32:03 +0200

    structuring project

---
+++ Oetools.Packager.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Oetools.Packager.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1() {
+            Assert.IsTrue(true);
+        }
+    }
+}
---
+++ Oetools.Packager/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Oetools.Packager
+{
+    public class Main {
+
+        public static void Run() {
+            Console.WriteLine("Running packager");
+            Utilities.Poc1.Hello();
+        }
+    }
+}
